695597b348ee77f43f18789be0035e35ce7d1112
Atomarverseucht <atomcraft.4u@web.de>
2026-04-15 02:16:57 +0200

    fix: ...

--- src/client/room/script.js
+++ src/client/room/script.js
@@ -2,7 +2,6 @@ let tb_player = new Map();
 let g_pl = "";
 let oldInnerHTML = "";
 
-
 function initLeaderboard() {
   let tblStart = "<table id='leaderboardID'>";
   let tblEnd = "</table>";
@@ -17,9 +16,7 @@ function uptLeaderboard() {
   function playerAsTB(v, key) {
     g_pl += "<tr id='new-player-id' ><td id='player-name' >" + key + "</td><td id='player-score' >" + v + "</td></tr>";
   }
-
   tb_player.forEach(playerAsTB);
-
   let leader_id = document.getElementById("leaderboard_p");
   leader_id.innerHTML = oldInnerHTML.replace("</table>", "") + g_pl + "</table>";
   g_pl = "";
