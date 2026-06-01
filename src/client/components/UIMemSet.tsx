3c3b8f6ea142c9a56c96fdbbd8ba866f9b048d0d
Atomarverseucht <atomcraft.4u@web.de>
2026-06-01 09:08:18 +0200

    fix: added a new test for room

--- src/client/components/UIMemSet.tsx
+++ src/client/components/UIMemSet.tsx
@@ -4,7 +4,7 @@ export default function UIMemSet({ memSet }: Props) {
     return (
         <section className="select-memory" id="theme">
             { memSet.map((mem: MemorySet) => (
-                <a className="memory-set" href="/room">
+                <a className="memory-set" href="/room/">
                     <img src={mem.titlePicture.picture} alt={mem.titlePicture.altText} />
                     <p>{mem.name}</p>
                 </a>
--- src/client/room/room.tsx
+++ src/client/room/room.tsx
@@ -1,7 +1,12 @@
 import {createRoot} from "react-dom/client";
 
 export function App(){
-    return <a> I am an example of a second page</a>;
+    return (<main>
+        <article>
+            <h2>Please select your THEME</h2>
+            <p>The themes are the indicator with which set you will be playing.</p>
+        </article>
+    </main>);
 }
 
 createRoot(document.getElementById("app")!).render(<App/>);
