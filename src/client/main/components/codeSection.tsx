33294297062d579092d4ad65931163ea7c927630
Atomarverseucht <atomcraft.4u@web.de>
2026-07-03 17:19:40 +0200

    feat: code bar does now work!

--- src/client/main/components/codeSection.tsx
+++ src/client/main/components/codeSection.tsx
@@ -1,9 +1,15 @@
+import {type NavigateFunction, useNavigate} from "react-router-dom";
+
 export function CodeSection() {
+    const nav = useNavigate()
     return (<article className="codeSection">
         <label htmlFor="code">Code:</label>
-        <input id="code" type="text" placeholder="roomkey" maxLength={6}/>
-        <button id="submitCode" onClick={nothing}>Submit</button>
+        <input id="code" type="text" placeholder="roomkey" maxLength={4}/>
+        <button id="submitCode" onClick={() => linkToRoom(nav)}>Submit</button>
     </article>);
 }
 
-function nothing() {}
+function linkToRoom(nav: NavigateFunction) {
+    console.log("hi");
+    nav("/room/?roomID=" + (document.getElementById("code") as HTMLInputElement).value );
+}
