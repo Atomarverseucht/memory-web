4dd117e2a1170853a89eaefd8022be9099890cb9
Timothy <timo-drexler@online.de>
2026-07-28 14:16:37 +0200

    Removed some comments.

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,17 @@ import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [react()],
-    // Vite schaut jetzt primär in den Ordner src/client/
     root: 'src/client',
     server: {
         proxy: {
-            '/api': 'http://localhost:3000',  // API-Requests an Express
+            '/api': 'http://localhost:3000',
         },
     },
     build: {
-        // Da das Rootverzeichnis verschoben wurde, bauen wir den Output
-        // drei Ebenen höher in das Hauptverzeichnis (dist-client)
         outDir: resolve(__dirname, 'dist-client'),
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                // Die Pfade sind nun relativ zu src/client/
                 main: resolve(__dirname, 'src/client/index.html')
             },
         },
